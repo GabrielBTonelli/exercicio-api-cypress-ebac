71abf3ad2d8ec1a0124373589637a0a6e51890ca
GabrielBTonelli <tonelli.gabriel@gmail.com>
2025-08-23 12:21:06 +0200

    Revisao I

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 import contrato from '../contratos/produtos.contratos'
+import { faker } from "@faker-js/faker";
 
 describe('Testes da Funcionalidade Usuários', () => {
-
   let token    
     beforeEach(() => {
         cy.token('fulano@qa.com', 'teste').then(tkn => {
@@ -28,9 +28,13 @@ describe('Testes da Funcionalidade Usuários', () => {
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-     let nome = 'ALUNO EBAC ' + Math.floor(Math.random() * 1000000000)
-     let email = 'aluno' + Math.floor(Math.random() * 1000000000) + '@qa.com.br'
-     cy.cadastrarUsuario(token, nome, email, 'teste', 'true').should((response) =>{
+    //  let nome = 'ALUNO EBAC ' + Math.floor(Math.random() * 1000000000)
+    //  let email = 'aluno' + Math.floor(Math.random() * 1000000000) + '@qa.com.br'
+    var nome = faker.person.firstName()
+    var email = faker.internet.email(nome)
+    var senha = faker.internet.password()
+
+    cy.cadastrarUsuario(token, nome, email, senha, 'true').should((response) =>{
       expect(response.status).equal(201)
       expect(response.body.message).equal('Cadastro realizado com sucesso')
      })
@@ -39,25 +43,41 @@ describe('Testes da Funcionalidade Usuários', () => {
   it('Deve validar um usuário com email inválido', () => {
     let nome = 'ALUNO EBAC ' + Math.floor(Math.random() * 1000000000)
     let email = nome + '@qa.com.br'
-    cy.cadastrarUsuario(token, nome, email, 'teste', 'true').should((response) =>{
+    var senha = faker.internet.password()
+    
+    cy.cadastrarUsuario(token, nome, email, senha, 'true').should((response) =>{
       expect(response.status).equal(400)
       expect(response.body.email).contains('email deve ser um email válido')
      }) 
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-      cy.editarUsuario('HsajixttWjoFo8Ve', token, 'Beltrano Silva', 'beltranoS@ebac.com.br', 'teste', 'false')
+    var nome = faker.person.firstName()
+    var email = faker.internet.email(nome)
+
+    cy.cadastrarUsuario(token, nome, email, 'teste', 'true').then((response) =>{
+      let id = response.body._id
+      
+      cy.editarUsuario(id, token, nome, email, 'teste', 'false')
       .then((response) =>{
         expect(response.status).equal(200)
         expect(response.body.message).equal('Registro alterado com sucesso')
      })
+    })
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    cy.deletarUsuario('uTU2IPij2CKlff1G')
-    .then((response) =>{
-        expect(response.status).equal(200)
-        expect(response.body.message).equal('Registro excluído com sucesso')
-    })
+    var nome = faker.person.firstName()
+    var email = faker.internet.email(nome)
+
+    cy.cadastrarUsuario(token, nome, email, 'teste', 'true').then((response) =>{
+      let id = response.body._id
+      
+      cy.deletarUsuario(id)
+      .then((response) =>{
+          expect(response.status).equal(200)
+          expect(response.body.message).equal('Registro excluído com sucesso')
+      })
+      })
   });
 });
